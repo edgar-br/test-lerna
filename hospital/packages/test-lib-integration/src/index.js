67d412ba4fb09b0aa640c629c58f4b6425f87cc1
Edgar Perez <edgar@cbqasolutions.com>
2022-03-24 10:42:21 -0600

    Testing publishing

---
+++ hospital/packages/test-lib-integration/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var doctor_1 = require("@hospital-sdk/doctor");
+var patient_1 = require("@hospital-sdk/patient");
+var scheduler_1 = require("@hospital-sdk/scheduler");
+(0, doctor_1.message)();
+(0, patient_1.message)();
+(0, scheduler_1.message)();
---
+++ hospital/packages/test-lib-integration/src/index.ts
@@ -0,0 +1,7 @@
+import { message as doctorMessage } from '@hospital-sdk/doctor';
+import { message as patientMessage } from '@hospital-sdk/patient';
+import { message as schedulerMessage } from '@hospital-sdk/scheduler';
+
+doctorMessage();
+patientMessage();
+schedulerMessage();
